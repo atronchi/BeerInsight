f39a94672021b18f540057a690307049efdfc5c3
Alexander Tronchin-James <alex.n.james@gmail.com>
2013-09-16 22:04:46 -0700

    Added forms, typeahead, and more. RR closing... need to run!

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,25 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
+from wtforms import TextField, DecimalField, BooleanField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
-    remember_me = BooleanField('remember_me', default = False)
+    openid = TextField('openid', 
+        validators = [Required()]
+        )
+    remember_me = BooleanField('remember_me', 
+        default = False
+        )
+
+class UserInputForm(Form):
+    brewer = TextField('brewer',
+        default='Brewer Name', 
+        validators = [Required()]
+        )
+    beer = TextField('beer',
+        default='Beer Name', 
+        validators = [Required()]
+        )
+    rating = DecimalField('rating', places=0, default=10,
+        validators = [Required()]
+        )
 
--- app/views.py
+++ app/views.py
@@ -1,28 +1,24 @@
-from flask import render_template
+from flask import render_template,request
 from app import app
 import os
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("home.html",
-        title = 'BeerSuggest',
-        )
+    return render_template("home.html",footer_color='white')
 
 @app.route('/about')
 def about():
     slides = ['static/slides/'+i for i in sorted([a[2] for a in os.walk('app/static/slides')][0])]
     idx = range(len(slides))
     return render_template("about.html",
-        title = 'BeerSuggest',
-        slides = slides, idx = idx
+        slides = slides, idx = idx,
+        footer_color='white'
         )
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html",
-        title = 'BeerSuggest'
-        )
+    return render_template("contact.html",footer_color='white')
 
 
 # Map of beer locations
@@ -33,39 +29,56 @@ with gzip.open('scraper/scrape_ratebeer.pklz','rb') as f: loc_data=cPickle.load(
 def rd_sanjose():
     return json.dumps(loc_data['locations'])
 
-with gzip.open('app/gapi_key.pklz','rb') as f: gapi=cPickle.load(f)
-@app.route('/maps')
-def maps():
-    return render_template("google-maps.html",
-        title = 'BeerSuggest',
-        key = gapi['key']
-        )
-
 
 # Forms for login and user beer preference selection
 # see http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
 # see http://twitter.github.io/typeahead.js/examples/
 from flask import flash, redirect
-from forms import LoginForm
+from forms import LoginForm,UserInputForm
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', 
-        title = 'Sign In',
-        form = form)
+    return render_template('login.html', footer_color='white',
+        form = LoginForm())
+
+@app.route('/user_input', methods = ['GET', 'POST'])
+def user_input():
+    return render_template('user_input.html', footer_color='white',
+        form = UserInputForm())
+
+# load BA beer data to import to forms autofill
+import gzip,cPickle
+with gzip.open('recommender/reviews.pklz2') as f: BA=cPickle.load(f)
+BA_brewers = list(set([b[0] for b in BA['beers']]))[0:1000]
+BA_beers = list(set([b[1] for b in BA['beers']]))
+@app.route('/brewers')
+def brewers():
+    query = request.args.get('q','')
+    print query
+    if query=='':
+        return json.dumps(BA_brewers[0:1000])
+    else:
+        return json.dumps(BA_brewers)
+@app.route('/beers')
+def beers():
+    return json.dumps(BA_beers)
+
+
+with gzip.open('app/gapi_key.pklz','rb') as f: gapi=cPickle.load(f)
+@app.route('/maps')
+def maps():
+    return render_template("maps.html",footer_color='black',
+        key = gapi['key'],
+        form = UserInputForm())
 
 
 # views for testing/debugging
 @app.route('/test') 
 def test():
-    return render_template("divtest.html",
-        title = 'BeerSuggest'
-        )
+    return render_template("divtest.html")
 
 @app.route('/debug')
 def debug():
     return render_template("debug.html",
-        title = 'BeerSuggest',
         slides = ['static/slides/'+i for i in sorted([a[2] for a in os.walk('app/static/slides')][0])],
         pwd=os.getcwd()
         )
